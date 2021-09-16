b44ba98515fa3b9e9d51801b112f99f4bc37a54c
lijeffrey02 <lijeffrey02@gmail.com>
2021-09-16 15:19:52 -0400

    Finalize comments

--- points2d.h
+++ points2d.h
@@ -13,14 +13,9 @@
 
 namespace teaching_project {
 
-// Place comments that provide a brief explanation of the class,
-// and its sample usage.
 template<typename Object>
 class Points2D {
  public:
-  // Default "big five" -- you have to alter them for your assignment.
-  // That means that you will remove the "= default" statement.
-  //  and you will provide an implementation.
 
   // Zero-parameter constructor. 
   // Set size to 0.
@@ -41,9 +36,7 @@ class Points2D {
     }
   }
 
-  // Copy-assignment. If you have already written
-  // the copy-constructor and the move-constructor
-  // you can just use:
+  // Copy-assignment
   Points2D& operator=(const Points2D &rhs)
   {
   Points2D copy = rhs; 
@@ -60,7 +53,6 @@ class Points2D {
   }
 
   // Move-assignment.
-  // Just use std::swap() for all variables.
   Points2D& operator=(Points2D &&rhs){
     std::swap(sequence_, rhs.sequence_);
     std::swap(size_, rhs.size_);
@@ -91,7 +83,7 @@ class Points2D {
     int size_of_sequence;
     input_stream >> size_of_sequence;
     // Allocate space for sequence.
-    // Add code here.
+    
     size_ = size_of_sequence;
     sequence_ = new std::array<Object, 2>[size_of_sequence];
     
