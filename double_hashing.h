d6eacd20a4d7dab8288e6df31c2176b52bbbbc66
lijeffrey02 <64700348+lijeffrey02@users.noreply.github.com>
2021-11-03 15:59:04 -0400

    Init

---
+++ double_hashing.h
@@ -0,0 +1,12 @@
+#ifndef DOUBLE_HASHING_H
+#define DOUBLE_HASHING_H
+
+#include <vector>
+#include <algorithm>
+#include <functional>
+
+template <typename HashedObj>
+class HashTable {
+    public:
+
+}
---
+++ linear_probing.h
@@ -0,0 +1,12 @@
+#ifndef LINEAR_PROBING_H
+#define LINEAR_PROBING_H
+
+#include <vector>
+#include <algorithm>
+#include <functional>
+
+template <typename HashedObj>
+class HashTable {
+    public:
+
+}
