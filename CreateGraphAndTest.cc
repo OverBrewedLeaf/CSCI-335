92e7bd7f01c9492eb1b7478a94d0334599c6c1e6
Jeffrey Li <lijeffrey02@gmail.com>
2021-12-10 22:39:18 -0500

    q1

--- CreateGraphAndTest.cc
+++ CreateGraphAndTest.cc
@@ -1,15 +1,63 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <fstream>
+#include <sstream>
+
 
 #include "graph.h"
 
 using namespace std;
 
+template <typename DistType>
 int graphTestDriver(int argc, char **argv) {
     
     //Begin your code here. Feel free to add any helper functions or classes you need,
     //as long as we only have to call this function to run the specified assignment.
+
+    ifstream input_file(argv[1]);
+    ifstream query_file(argv[2]);
+    string line;
+
+    getline(input_file,line);
+
+    Graph<DistType> graph(stoi(line));
+
+
+    while(getline(input_file,line))
+    {
+      stringstream stream(line);
+      int vertex = 0;
+      int vertexid = 1;
+      DistType weight = 2;
+
+      stream >> vertex;
+      
+      while(stream)
+      {
+        stream >> vertexid;
+        stream >> weight;
+        if(stream){
+          graph.insert(vertex,vertexid,weight);
+        }
+      }
+    }
+    // std::cout << "after inserting Graph is" << std::endl;
+    // graph.printGraph();
+
+    while(getline(query_file,line))
+    {
+      int start, end;
+      stringstream stream(line);
+      stream >> start;
+      stream >> end;
+      std::cout << line << ": ";
+      graph.connection(start,end);
+      std::cout << std::endl;
+    }
+    // std::cout << "later Graph is " << std::endl;
+    // graph.printGraph();
+
 
     return 0;
 }
@@ -20,7 +68,7 @@ int main(int argc, char **argv) {
 		return 0;
     }
 
-    graphTestDriver(argc, argv);
+    graphTestDriver<float>(argc, argv);
 
     return 0;
 }
--- graph.h
+++ graph.h
@@ -3,11 +3,102 @@
 
 #include "binary_heap.h"
 
+#include <iomanip>
+#include <iostream>
+
 //This file is for your graph implementation.
 //Add everything you need in between the "ifndef and "endif" statements.
 //Do not put anything outside those statements
 
+template <typename DistType>
+class Vertex
+{
+    private:
+    std::vector<Vertex<DistType>> adjacent_;
+    DistType distance_;//currently same as weight
 
+
+    public: 
+    Vertex()
+    {
+        distance_ = 0;
+    }
+
+    Vertex(DistType distance)
+    {
+        distance_ = distance;
+    }
+
+    DistType getDistance()
+    {
+        return distance_;
+    }
+
+    void insert(size_t vertex, DistType distance)
+    {
+        // std::cout << "inside insert function vertex- " << vertex << "   distance- " << distance << std::endl;
+        if(adjacent_.size() <= vertex)
+        {
+            adjacent_.resize(vertex+2);
+        }
+        // std::cout << "\t adjacent size is " << adjacent_.size() << std::endl;
+        adjacent_[vertex] = Vertex<DistType>(distance);
+        // adjacent_.insert(vertex,Vertex<DistType>(distance));
+        // std::cout << "inside insert adjacent_ is " << adjacent_.size() << std::endl;
+    }
+    string printVertex(){
+        string result = "distance: " + to_string(distance_) + "\n";
+        for (size_t i = 0; i < adjacent_.size(); i++){
+            result += "\tadjacent of " + to_string(i) + " has distance of " + to_string(adjacent_[i].distance_) + "\n";
+        }
+        return result;
+    }
+
+    void adjacent(size_t end)
+    {
+        // std::cout << "the adjacent_ size is " << adjacent_.size() << std::endl;
+        // std::cout << printVertex() << std::endl;
+        if (adjacent_.size() < end)
+            std::cout << "not_connected ";
+        else if(adjacent_[end].getDistance() != 0)
+            std::cout << "connected " << adjacent_[end].getDistance();
+        else std::cout << "not_connected ";
+    }
+
+} ;
+
+template <typename DistType>
+class Graph
+{
+    private:
+    Vertex<DistType> *vertex_;
+    int size_;
+
+    public:
+    Graph(size_t size)
+    {
+        vertex_ = new Vertex<DistType>[size+1];
+        size_ = size;
+    }
+    ~Graph(){};
+
+    void insert(int vertex, int vertexid, DistType distance)
+    {
+        vertex_[vertex].insert(vertexid,distance);
+    }
+    void printGraph(){
+        std::cout << "entered print graph " << std::endl;
+        std::cout << "size is " << size_ << std::endl;
+        for(int i=0; i<size_; i++){
+            std::cout << "i = " << i << std::endl;
+            std::cout << vertex_[i].printVertex() << std::endl;
+        }
+    }
+    void connection(int start,int end)
+    {
+        return vertex_[start].adjacent(end);
+    }
+};
 
 
 
